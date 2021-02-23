3fd2a1ce5a01f94f79929eeb937284fcd297efad
Rakhi <rakhi.nagpal16@gmail.com>
2021-02-23 12:37:37 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -4,11 +4,12 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform1;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
+
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -17,7 +18,9 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+    platform1 = new Ground(150, 305, 300, 170);
+    
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -60,5 +63,5 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
+   platform1.display();
 }
